7b7dbf36ca0d93f2d3e34ceb296869a8509d7111
JasonDen <jason.den.cloud@gmail.com>
2020-10-19 12:17:32 +1100

    fix visual bug

--- src/components/ToggleSwitch/ToggleSwitch.js
+++ src/components/ToggleSwitch/ToggleSwitch.js
@@ -1,22 +1,23 @@
-import React, { useState, useContext } from "react";
-import StyleContext from "../../contexts/StyleContext";
-import "./ToggleSwitch.css";
+import React, { useState, useContext } from "react"
+import StyleContext from "../../contexts/StyleContext"
+import "./ToggleSwitch.css"
 const ToggleSwitch = () => {
-  const [isChecked, setChecked] = useState(false);
-  const styleContext = useContext(StyleContext);
+  const [isChecked, setChecked] = useState(false)
+  const styleContext = useContext(StyleContext)
 
   return (
     <label className="switch">
       <input
+        className="hidden"
         type="checkbox"
         checked={isChecked}
         onChange={() => {
-          styleContext.changeTheme();
-          setChecked(!isChecked);
+          styleContext.changeTheme()
+          setChecked(!isChecked)
         }}
       />
       <span className="slider round"></span>
     </label>
-  );
-};
-export default ToggleSwitch;
+  )
+}
+export default ToggleSwitch
